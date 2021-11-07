f95bdd273aad8c720aa2e01ef4a70a495dddd952
Blair Pattison <blair.pattis@gmail.com>
2021-11-07 10:22:16 -0600

    added mouse listener for the quiz grade calculation and display

--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -4,20 +4,31 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import edu.westga.cs.schoolgrades.model.CompositeGrade;
 import edu.westga.cs.schoolgrades.model.Grade;
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
+import edu.westga.cs.schoolgrades.model.SumOfGradesStrategy;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
+import javafx.util.converter.NumberStringConverter;
 
 /**
  * @author windy
@@ -44,6 +55,12 @@ public class SchoolGradesController implements Initializable {
 	@FXML
 	private MenuItem addExamGrade;
 
+	@FXML
+	private TextArea quizSubtotal;
+	@FXML
+	private Button calculateGradeButton;
+	private CompositeGrade quizGrades;
+
 	public void SchoolGradesControlleR() {
 		this.dataMenuButton = new MenuButton();
 		this.examsList = new ListView<>();
@@ -53,6 +70,8 @@ public class SchoolGradesController implements Initializable {
 		this.addQuizGrade = new MenuItem();
 		this.addExamGrade = new MenuItem();
 		this.addHwGrade = new MenuItem();
+		this.quizSubtotal = new TextArea();
+
 
 	}
 
@@ -61,70 +80,90 @@ public class SchoolGradesController implements Initializable {
 		this.hwGradesList = FXCollections.observableArrayList();
 		this.examGradesList = FXCollections.observableArrayList();
 		this.quizGradesList = FXCollections.observableArrayList();
+		this.quizGrades = new CompositeGrade(new SumOfGradesStrategy());
 
 		this.buildQuizList();
 		this.buildExamList();
 		this.buildHwList();
+		this.calculateGradeButton.setOnMousePressed(new CalculateGrade());
+		
 
 	}
 
 	private void buildExamList() {
 		this.examsList.setItems(this.examGradesList);
+		this.examsList.setEditable(true);
 		this.examsList.setCellFactory(lv -> {
-		    TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
-		    cell.setConverter(new GradeConverter(cell));
-		    return cell ;
-			
+			TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
+			cell.setConverter(new GradeConverter(cell));
+			return cell;
+
 		});
 		this.addExamGrade.setOnAction(event -> {
-		    this.examGradesList.add(new SimpleGrade(0));
+			this.examGradesList.add(new SimpleGrade(0));
 		});
 	}
-	
+
 	private void buildHwList() {
 		this.hwList.setItems(this.hwGradesList);
+		this.hwList.setEditable(true);
 		this.hwList.setCellFactory(lv -> {
-		    TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
-		    cell.setConverter(new GradeConverter(cell));
-		    return cell ;
-			
+			TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
+			cell.setConverter(new GradeConverter(cell));
+			return cell;
+
 		});
 		addHwGrade.setOnAction(event -> {
-		    this.hwGradesList.add(new SimpleGrade(0));
+			this.hwGradesList.add(new SimpleGrade(0));
 		});
 	}
 
 	private void buildQuizList() {
 		this.quizzesList.setItems(this.quizGradesList);
+		this.quizzesList.setEditable(true);
 		this.quizzesList.setCellFactory(lv -> {
-		    TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
-		    cell.setConverter(new GradeConverter(cell));
-		    return cell ;
-			
+			TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
+			cell.setConverter(new GradeConverter(cell));
+			return cell;
+
 		});
 		addQuizGrade.setOnAction(event -> {
-		    this.quizGradesList.add(new SimpleGrade(0));
+			this.quizGradesList.add(new SimpleGrade(0));
 		});
 	}
 
 	public static class GradeConverter extends StringConverter<SimpleGrade> {
-        private final ListCell<SimpleGrade> cell;
-        public GradeConverter(ListCell<SimpleGrade> cell) {
-            this.cell = cell ;
-        }
-        @Override
-        public String toString(SimpleGrade grade) {
-            return String.valueOf(grade.getValue());
-        }
+		private final ListCell<SimpleGrade> cell;
 
-        @Override
-        public SimpleGrade fromString(String string) {
-            SimpleGrade grade = cell.getItem();
-            grade.setValue(Double.parseDouble(string));
-            return grade;
-        }
-	
+		public GradeConverter(ListCell<SimpleGrade> cell) {
+			this.cell = cell;
+		}
 
-    }
+		@Override
+		public String toString(SimpleGrade grade) {
+			return String.valueOf(grade.getValue());
+		}
 
+		@Override
+		public SimpleGrade fromString(String string) {
+			SimpleGrade grade = cell.getItem();
+			grade.setValue(Double.parseDouble(string));
+			return grade;
+		}
+
+	}
+
+	private class CalculateGrade implements EventHandler<MouseEvent> {
+		public void handle(MouseEvent me) {
+			quizGrades = new CompositeGrade(new SumOfGradesStrategy());
+			quizGrades.addAll(quizGradesList);
+			if (quizGrades == null) {
+				quizSubtotal.setText("0");
+			} else {
+				
+				quizSubtotal.setText(String.valueOf(quizGrades.getValue()));
+			}
+		}
+
+	}
 }
