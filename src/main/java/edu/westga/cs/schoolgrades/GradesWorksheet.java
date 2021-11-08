f66c0a4a7afd0f3bd310da133b4fac5f2c9b7a49
Blair Pattison <blair.pattis@gmail.com>
2021-11-08 05:07:52 -0600

    added private helper methods for more streamlined
    code in controller

--- src/main/java/edu/westga/cs/schoolgrades/GradesWorksheet.java
+++ src/main/java/edu/westga/cs/schoolgrades/GradesWorksheet.java
@@ -9,31 +9,39 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Starting point for application.
+ * 
+ * @author Blair Pattison
+ * @version 11/08/2021
+ *
+ */
+
 public class GradesWorksheet extends Application {
 	private static final String GUI_RESOURCE = "edu/westga/cs/schoolgrades/views/SchoolGradesGUI.fxml";
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-    	
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        URL resource = classLoader.getResource(GradesWorksheet.GUI_RESOURCE);
-        FXMLLoader loader = new FXMLLoader(resource);
-        loader.setController(new SchoolGradesController());
-        Parent root = (Parent) loader.load();
-       
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Grades Worksheet");
-        primaryStage.show();
-    }
 
-    /**
-     * Start point for the appliction.
-     * 
-     * @param args not used
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		URL resource = classLoader.getResource(GradesWorksheet.GUI_RESOURCE);
+		FXMLLoader loader = new FXMLLoader(resource);
+		loader.setController(new SchoolGradesController());
+		Parent root = (Parent) loader.load();
+
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Grades Worksheet");
+		primaryStage.show();
+	}
+
+	/**
+	 * Start point for the application.
+	 * 
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
--- src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -1,41 +1,39 @@
 package edu.westga.cs.schoolgrades.controllers;
 
 import java.net.URL;
-import java.util.ArrayList;
+
 import java.util.ResourceBundle;
 
 import edu.westga.cs.schoolgrades.model.AverageOfGradesStrategy;
 import edu.westga.cs.schoolgrades.model.CompositeGrade;
 import edu.westga.cs.schoolgrades.model.DropLowestStrategy;
-import edu.westga.cs.schoolgrades.model.Grade;
+
 import edu.westga.cs.schoolgrades.model.SimpleGrade;
 import edu.westga.cs.schoolgrades.model.SumOfGradesStrategy;
 import edu.westga.cs.schoolgrades.model.WeightedGrade;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.Alert.AlertType;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.input.MouseEvent;
-import javafx.util.Callback;
 import javafx.util.StringConverter;
-import javafx.util.converter.NumberStringConverter;
 
 /**
- * @author windy
- *
+ * This class is the calculates and displays the GradesWorksheet gui.
+ * 
+ * @author Blair Pattison
+ * @version 11/08/2021
+ * 
  */
 public class SchoolGradesController implements Initializable {
 	@FXML
@@ -75,15 +73,19 @@ public class SchoolGradesController implements Initializable {
 
 	public void SchoolGradesControlleR() {
 		this.dataMenuButton = new MenuButton();
+
 		this.examsList = new ListView<>();
 		this.hwList = new ListView<>();
 		this.quizzesList = new ListView<>();
-		this.dataMenuButton = new MenuButton();
+
 		this.addQuizGrade = new MenuItem();
 		this.addExamGrade = new MenuItem();
 		this.addHwGrade = new MenuItem();
+
 		this.quizSubtotal = new TextArea();
 		this.finalGradeSubtotal = new TextArea();
+		this.examSubtotal = new TextArea();
+		this.hwSubtotal = new TextArea();
 
 	}
 
@@ -92,59 +94,42 @@ public class SchoolGradesController implements Initializable {
 		this.hwGradesList = FXCollections.observableArrayList();
 		this.examGradesList = FXCollections.observableArrayList();
 		this.quizGradesList = FXCollections.observableArrayList();
+
 		this.quizGrades = new CompositeGrade(new SumOfGradesStrategy());
 		this.homeworkGrades = new CompositeGrade(new DropLowestStrategy(new AverageOfGradesStrategy()));
 		this.examGrades = new CompositeGrade(new AverageOfGradesStrategy());
 
-		this.buildQuizList();
-		this.buildExamList();
-		this.buildHwList();
+		this.buildListView(this.quizzesList, this.quizGradesList, this.addQuizGrade);
+		this.buildListView(this.examsList, this.examGradesList, this.addExamGrade);
+		this.buildListView(this.hwList, this.hwGradesList, this.addHwGrade);
+
 		this.calculateGradeButton.setOnMousePressed(new CalculateGrade());
 
 	}
 
-	private void buildExamList() {
-		this.examsList.setItems(this.examGradesList);
-		this.examsList.setEditable(true);
-		this.examsList.setCellFactory(lv -> {
+	/**
+	 * Displays the list view for the quizzes.
+	 */
+	private void buildListView(ListView<SimpleGrade> list, ObservableList<SimpleGrade> grades, MenuItem menu) {
+		list.setItems(grades);
+		list.setEditable(true);
+		list.setCellFactory(lv -> {
 			TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
 			cell.setConverter(new GradeConverter(cell));
 			return cell;
-
 		});
-		this.addExamGrade.setOnAction(event -> {
-			this.examGradesList.add(new SimpleGrade(0));
-		});
-	}
-
-	private void buildHwList() {
-		this.hwList.setItems(this.hwGradesList);
-		this.hwList.setEditable(true);
-		this.hwList.setCellFactory(lv -> {
-			TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
-			cell.setConverter(new GradeConverter(cell));
-			return cell;
-
-		});
-		addHwGrade.setOnAction(event -> {
-			this.hwGradesList.add(new SimpleGrade(0));
+		menu.setOnAction(event -> {
+			grades.add(new SimpleGrade(0));
 		});
 	}
 
-	private void buildQuizList() {
-		this.quizzesList.setItems(this.quizGradesList);
-		this.quizzesList.setEditable(true);
-		this.quizzesList.setCellFactory(lv -> {
-			TextFieldListCell<SimpleGrade> cell = new TextFieldListCell<>();
-			cell.setConverter(new GradeConverter(cell));
-			return cell;
-
-		});
-		addQuizGrade.setOnAction(event -> {
-			this.quizGradesList.add(new SimpleGrade(0));
-		});
-	}
-
+	/**
+	 * Converts and calculates the Grade's value into a string to be editable by the
+	 * user in the ListView's cell factory.
+	 * 
+	 * @author Blair Pattison
+	 *
+	 */
 	public static class GradeConverter extends StringConverter<SimpleGrade> {
 		private final ListCell<SimpleGrade> cell;
 
@@ -166,6 +151,13 @@ public class SchoolGradesController implements Initializable {
 
 	}
 
+	/**
+	 * Controls the Calculate Grade button and displays the subsequent grade
+	 * subtotals and final totals.
+	 * 
+	 * @author Blair Pattison
+	 *
+	 */
 	private class CalculateGrade implements EventHandler<MouseEvent> {
 		public void handle(MouseEvent me) {
 			quizGrades = new CompositeGrade(new SumOfGradesStrategy());
@@ -176,36 +168,40 @@ public class SchoolGradesController implements Initializable {
 
 			examGrades = new CompositeGrade(new AverageOfGradesStrategy());
 			examGrades.addAll(examGradesList);
-			
+
 			subtotalCalculator(quizGrades, quizSubtotal);
 			subtotalCalculator(homeworkGrades, hwSubtotal);
 			subtotalCalculator(examGrades, examSubtotal);
-			
+
 			finalGradeCalculator();
 		}
 
 	}
-	
+
+	/**
+	 * Calculates the desired subtotal and displays it.
+	 * 
+	 * @param grade    the CompositeGrade to calculate
+	 * @param subtotal The subtotal textarea to display it in.
+	 */
 	private void subtotalCalculator(CompositeGrade grade, TextArea subtotal) {
 		if (grade == null) {
-			subtotal.setText("0");
+			subtotal.setText("0.0");
 		} else {
 			subtotal.setText(String.valueOf(grade.getValue()));
 		}
-		
 	}
-	
-	private void finalGradeCalculator() {		
+
+	/**
+	 * Calculates the final grade and displays it in the TextArea.
+	 */
+	private void finalGradeCalculator() {
 		String finalGrade;
-		if (quizGrades == null || homeworkGrades == null || examGrades == null) {
-			finalGrade = "0";
-		} else {
-			WeightedGrade finalGrade1 = new WeightedGrade(quizGrades, 0.2);
-			WeightedGrade finalGrade2 = new WeightedGrade(homeworkGrades, 0.3);
-			WeightedGrade finalGrade3 = new WeightedGrade(examGrades, 0.5);
-			finalGrade = String.valueOf(finalGrade1.getValue() + finalGrade2.getValue() + finalGrade3.getValue());
-		}
-		
+		WeightedGrade finalGrade1 = new WeightedGrade(quizGrades, 0.2);
+		WeightedGrade finalGrade2 = new WeightedGrade(homeworkGrades, 0.3);
+		WeightedGrade finalGrade3 = new WeightedGrade(examGrades, 0.5);
+		double finalGradeDouble = finalGrade1.getValue() + finalGrade2.getValue() + finalGrade3.getValue();
+		finalGrade = String.valueOf(finalGradeDouble);
 		finalGradeSubtotal.setText(finalGrade);
 	}
 
